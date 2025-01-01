ce0145df552f3c75e487a606a1c866ef7ddfd6ad
Walter Adrian Samuel <walter.adrian@student.umn.ac.id>
2025-01-02 00:35:57 +0700

    commit

--- app/src/main/java/com/example/momentia/DTO/Memory.kt
+++ app/src/main/java/com/example/momentia/DTO/Memory.kt
@@ -4,10 +4,8 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.GeoPoint
 
 data class Memory(
-    val location: GeoPoint? = null,
     val mediaUrl: String = "",
     val receiverId: String = "",
     val senderId: String = "",
-    val sentAt: Timestamp? = null,
-    val viewed: Boolean = false
+    val sentAt: Timestamp? = null
 )
